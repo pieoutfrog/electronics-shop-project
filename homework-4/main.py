2f6289ba018e6db0ca1d8d5b476b678dca5d9a7f
Alice <despero45@gmail.com>
2023-04-24 09:50:53 +1000

    Дз-4

--- homework-4/main.py
+++ homework-4/main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert str(phone1) == 'iPhone 14'
     assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
-    assert phone1.number_of_sim == 2
+    assert phone1.sim_cards_num == 2
 
     item1 = Item("Смартфон", 10000, 20)
     assert item1 + phone1 == 25
